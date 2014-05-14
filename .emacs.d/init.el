8020dc2f46fc8450037d538e7e5139a0af4d54e7
Shigehiro Yanbe <shigehiro.yanbe@gmail.com>
2014-05-14 10:38:12 +0900

    セッションとか追加

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -2,6 +2,29 @@
 (set-language-environment "Japanese")
 (prefer-coding-system 'utf-8)
 
+;; ロードパスを追加
+(setq load-path
+  (cons (expand-file-name "~/.emacs.d/lisp") load-path)
+  )
+
+;; ミニバッファ履歴リストの最大長：tなら無限
+;(setq history-length t)
+(setq history-length 30)
+;; session.el
+;;   kill-ringやミニバッファで過去に開いたファイルなどの履歴を保存する
+(when (require 'session nil t)
+  (setq session-initialize '(de-saveplace session keys menus places)
+        session-globals-include '((kill-ring 50)
+                                  (session-file-alist 500 t)
+                                  (file-name-history 10000)))
+  (add-hook 'after-init-hook 'session-initialize)
+  ;; 前回閉じたときの位置にカーソルを復帰
+  (setq session-undo-check -1))
+
+;; minibuf-isearch
+;;   minibufでisearchを使えるようにする
+(require 'minibuf-isearch nil t)
+
 ;; C-hをDELキーに
 (keyboard-translate ?\C-h ?\C-?)
 
@@ -107,3 +130,15 @@
 ;(define-key c++-mode-map ?\: nil)
 
 
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(session-use-package t nil (session)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
